a2c17d49fe3f4ff307c1a9fb418a8e08cfa69c5e
omkarkulkarni112 <omkarkulkarni112@gmail.com>
2018-03-04 17:56:50 +0530

    Add project files.

---
+++ 1.1 Is Unique/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._1_Is_Unique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Is string '' having unique characters = {AreAllCharactersUnique("")}");
+            Console.WriteLine($"Is string '\" \"' having unique characters = {AreAllCharactersUnique(" ")}");
+            Console.WriteLine($"Is string 'a' having unique characters = {AreAllCharactersUnique("a")}");
+            Console.WriteLine($"Is string 'ab' having unique characters = {AreAllCharactersUnique("ab")}");
+            Console.WriteLine($"Is string 'aab' having unique characters = {AreAllCharactersUnique("aab")}");
+            Console.WriteLine($"Is string 'ab b' having unique characters = {AreAllCharactersUnique("ab b")}");
+            Console.WriteLine($"Is string 'abcdef gfhr' having unique characters = {AreAllCharactersUnique("abcdef gfhr")}");
+            Console.WriteLine($"Is string 'abcdef ghr' having unique characters = {AreAllCharactersUnique("abcdef ghr")}");
+
+            Console.WriteLine("Enter string to be checked for having all unique characters:");
+            string input = Console.ReadLine();
+
+            Console.WriteLine($"Is string '{input}' having unique characters = {AreAllCharactersUnique(input)}");
+
+            Console.ReadLine();
+        }
+
+        private static bool AreAllCharactersUnique(string input)
+        {
+            HashSet<char> charsInString = new HashSet<char>();
+
+            foreach (char c in input)
+            {
+                if (charsInString.Contains(c))
+                {
+                    return false;
+                }
+
+                charsInString.Add(c);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 1.2 Check Permutation/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._2_Check_Permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Is 'a' permutation of 'b' = {IsPermutation("a", "b")}");
+            Console.WriteLine($"Is 'a' permutation of 'aa' = {IsPermutation("a", "aa")}");
+            Console.WriteLine($"Is 'abcd' permutation of 'bcda' = {IsPermutation("abcd", "bcda")}");
+            Console.WriteLine($"Is 'ababab' permutation of 'aaabbb' = {IsPermutation("ababab", "aaabbb")}");
+            Console.WriteLine($"Is 'ababab' permutation of 'bbbaaa' = {IsPermutation("ababab", "bbbaaa")}");
+            Console.WriteLine($"Is 'lmnopqrst' permutation of 'mlontsqr' = {IsPermutation("lmnopqrst", "mlontsqr")}");
+            Console.WriteLine($"Is 'lmnopqrst' permutation of 'mlpontsqr' = {IsPermutation("lmnopqrst", "mlpontsqr")}");
+
+            Console.WriteLine("Enter input string 1:");
+            string input1 = Console.ReadLine();
+            Console.WriteLine("Enter input string 2:");
+            string input2 = Console.ReadLine();
+
+            Console.WriteLine($"Is '{input1}' permutation of '{input2}' = {IsPermutation(input1, input2)}");
+            Console.ReadLine();
+        }
+
+        private static bool IsPermutation(string input1, string input2)
+        {
+            if (input1.Length != input2.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, int> charCountTracker = new Dictionary<char, int>();
+
+            foreach (char c in input1)
+            {
+                if (charCountTracker.ContainsKey(c))
+                {
+                    charCountTracker[c]++;
+                }
+                else
+                {
+                    charCountTracker.Add(c, 1);
+                }
+            }
+
+            foreach (char c in input2)
+            {
+                if (charCountTracker.ContainsKey(c) && charCountTracker[c] > 0)
+                {
+                    charCountTracker[c]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+            // NO NEED TO CHECK FOR THE COUNTERS TO BE STILL GREATER THAN 1 
+            // SINCE THAT CANT HAPPEN AS WE HAVE EQUALL LENGTH STRINGS
+        }
+    }
+}
