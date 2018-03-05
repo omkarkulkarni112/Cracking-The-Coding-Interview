aeee96569e70468b38b65ade6a7599b254f85a83
omkarkulkarni112 <omkarkulkarni112@gmail.com>
2018-03-05 22:41:49 +0530

    Added in place matrix rotation method.

--- 1.7 Rotate Matrix/Program.cs
+++ 1.7 Rotate Matrix/Program.cs
@@ -10,11 +10,12 @@ namespace _1._7_Rotate_Matrix
     {
         static void Main(string[] args)
         {
-            int[,] matrix = new int[4, 4] {
-                                            { 10, 11, 12, 13 },
-                                            { 20, 21, 22, 23 },
-                                            { 30, 31, 32, 33 },
-                                            { 40, 41, 42, 43 }
+            int[,] matrix = new int[5, 5] {
+                                            { 11, 12, 13, 14, 15 },
+                                            { 21, 22, 23, 24, 25 },
+                                            { 31, 32, 33, 34, 35 },
+                                            { 41, 42, 43, 44, 45 },
+                                            { 51, 52, 53, 54, 55 }
                                         };
 
             Console.WriteLine("Rotating following matrix");
@@ -22,10 +23,34 @@ namespace _1._7_Rotate_Matrix
 
             int[,] rotatedMatrix = RotateMatrix90ToRight(matrix);
 
-            Console.WriteLine("Rotated matrix");
+            Console.WriteLine("\nRotated matrix (Not in place)");
             printMatrix(rotatedMatrix);
 
+            Console.WriteLine("\nRotated matrix (In place)");
+            RotateMatrix90ToRightInPlace(matrix);
+
             Console.ReadLine();
+        }
+
+        private static void RotateMatrix90ToRightInPlace(int[,] matrix)
+        {
+            int N = matrix.GetLength(0) - 1;
+            for (int i = 0; i < matrix.GetLength(0) / 2; i++)
+            {
+                for (int j = i; j < N - i; j++)
+                {
+                    int lastValue = matrix[N - j, i];
+
+                    matrix[N - j, i] = matrix[N - i, N - j];
+                    matrix[N - i, N - j] = matrix[j, N - i];
+                    matrix[j, N - i] = matrix[i, j];
+
+                    matrix[i, j] = lastValue;
+                }
+            }
+
+            printMatrix(matrix);
+
         }
 
         private static int[,] RotateMatrix90ToRight(int[,] matrix)
