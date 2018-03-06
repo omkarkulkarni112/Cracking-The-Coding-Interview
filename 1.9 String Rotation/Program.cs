29e9a351314e6d3a4cc066a2a79fe65f973c2d39
omkarkulkarni112 <omkarkulkarni112@gmail.com>
2018-03-06 22:07:34 +0530

    Added solution to problem 1.9 String Rotation

---
+++ 1.9 String Rotation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1._9_String_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"'waterbottle' is rotation of 'erbottlewat' => {IsRotationOf("waterbottle", "erbottlewat")}");
+            Console.WriteLine($"'waterbottle' is rotation of 'erbottletwa' => {IsRotationOf("waterbottle", "erbottletwa")}");
+
+            Console.ReadLine();
+        }
+
+        private static bool IsRotationOf(string str1, string str2)
+        {
+            return (str2 + str2).IndexOf(str1) >= 0;
+        }
+    }
+}
